8dd5fd70b8db918c3954141d997c2c44c060687d
Josh Fongheiser <Josh Fongheiser>
2017-02-17 22:24:48 -0500

    Add combat enter/exit text

--- ESOAddonTest.lua
+++ ESOAddonTest.lua
@@ -4,12 +4,30 @@ AddonTest = {}
 AddonTest.name = 'ESOAddonTest'
 
 function AddonTest:Initialize()
+	-- Keep track of whether the player is in combat.
+	self.inCombat = IsUnitInCombat('player')
 
+	-- Register event handlers.
+	EVENT_MANAGER:RegisterForEvent(self.name, EVENT_PLAYER_COMBAT_STATE, self.OnPlayerCombatState)
 end
 
+-- When addons are loaded, run our plugin's init function.
 function AddonTest.OnAddOnLoaded(event, addonName)
 	if addonName == AddonTest.name then
 		AddonTest:Initialize()
+	end
+end
+
+-- Handle when the player's combat state changes.
+function AddonTest.OnPlayerCombatState(event, inCombat)
+	if inCombat ~= AddonTest.inCombat then
+		AddonTest.inCombat = inCombat
+
+		if inCombat then
+			d('Entering Combat')
+		else
+			d('Exiting Combat')
+		end
 	end
 end
 
